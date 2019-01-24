5d5df0cd0948f24077aca31ea7d05ed94c3d1ffa
Nikolaos Bakoulas <46421526+NickBk92@users.noreply.github.com>
2019-01-24 18:54:03 +0100

    Update H3.jl

--- H3.jl
+++ H3.jl
@@ -14,16 +14,17 @@ include("handball_data.jl")
 (R,A) = size(referfee_arena_distances)
 (M,D) = size(match_time)
 T = size(team_match,1)
-
-arenaOccupied = zeros(A,D)
-
-for a=1:A
-    for d=1:D
-        arenaOccupied[a,d] = sum(match_areana[m,a]*match_time[m,d] for m=1:M)
+MM = 23
+team_match_ind=zeros(31,MM)
+for t=1:T
+    counter=1
+    for m=1:M
+        if team_match[t,m] == 1
+            team_match_ind[t,ct] = m
+            counter += 1
+        end
     end
 end
-
-
 
 
 #MODEL
@@ -34,7 +35,7 @@ H3 = Model(solver=GurobiSolver())
 #general sum of referee refereeing team t
 @variable(H3, y[r=1:R,t=1:T], Int)
 # sequential
-@variable(H3, z[r=1:R,t=1:T], Int)
+@variable(H3, z[r=1:R,t=1:T,d=1:D], Int)
 
 @objective(H3, Min, sum(x[r,m]*referfee_arena_distances[r,a]*match_areana[m,a] for r=1:R,m=1:M,a=1:A))
 
@@ -50,12 +51,14 @@ H3 = Model(solver=GurobiSolver())
 
 @constraint(H3, Sumrefteamcon[r=1:R,t=1:T], sum(x[r,m]*team_match[t,m] for m in 1:M) == y[r,t])
 
-@constraint(H3, sameteamtworounds[])
+
+@constraint(H3, sameteamtworounds[r=1:R,t=1:T,d=1:D],
+    sum(x[r,m]*match_time[m,d]*team_match[t,m] for m=1:M) == z[r,t,d])
 
 solve(H3)
 
 println(getobjectivevalue(H3))
 
-target_distance=getobjectivevalue(H3)
 
-println(getvalue(y[:,:]))
+
+println(sum(getvalue(z[51,t,d] )) for t=1:T,d=1:D)
