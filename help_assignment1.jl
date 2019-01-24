16790cada4999e6c1aad25702b7b4135d9a51d68
trantovard <tvardakas.1@gmail.com>
2019-01-24 14:14:01 +0100

    Update help_assignment1.jl

--- help_assignment1.jl
+++ help_assignment1.jl
@@ -17,7 +17,7 @@ end
 
 #Plan for every PUPIL, this one for p=178
 
-p=178
+p=12
 xres=getvalue(x)
 tikanei=zeros(3,4)
 for h=1:H
@@ -42,3 +42,41 @@ end
             end
             @printf "\n"
         end
+
+
+#PLAN for every teacher
+
+for t=1:T
+    zres=getvalue(z)
+    tikanei=zeros(3,4)
+    for h=1:H
+        for d=1:D
+            for c=1:C
+                if zres[d,h,c,t]==1
+                    tikanei[h,d]=c
+
+                end
+            end
+        end
+    end
+            @printf "\n\nTeacher: %d\n" t
+            for d=1:D
+                @printf "\t%d" d
+            end
+            @printf "\n"
+            for h=1:H
+                @printf "%d\t" h
+                for d=1:D
+                            @printf "%d\t" tikanei[h,d]
+                end
+                @printf "\n"
+            end
+end
+
+
+
+for p=1:P
+    if grade[p]==61
+        println(p)
+    end
+end
