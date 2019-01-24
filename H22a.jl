e92bfd6d13f1a9683c2eb130171ece59cd9a1f4f
trantovard <tvardakas.1@gmail.com>
2019-01-24 18:55:33 +0100

    Update H22a.jl

--- H22a.jl
+++ H22a.jl
@@ -50,4 +50,4 @@ solve(H22a)
 
 println(getobjectivevalue(H22a))
 
-println(sum(getvalue(x[r,m])*referfee_arena_distances[r,a]*match_areana[m,a] for r=1:R,m=1:M,a=1:A))
+target_pairs = getobjectivevalue(H22a)
