ca8c16c3bfb38902507fb645eefdd04eb7f2f0db
Nikolaos Bakoulas <46421526+NickBk92@users.noreply.github.com>
2019-01-25 04:31:36 +0100

    Update H21a.jl

--- H21a.jl
+++ H21a.jl
@@ -33,16 +33,16 @@ H21a = Model(solver=GurobiSolver())
 @variable(H21a, x[r=1:R,m=1:M], Bin)
 
 @objective(H21a, Min, sum(x[r,m]*referfee_arena_distances[r,a]*match_areana[m,a] for r=1:R,m=1:M,a=1:A))
-
+#Exactly two referees per match
 @constraint(H21a,twoRefPerMatch[m=1:M],
     sum(x[r,m] for r=1:R) == 2)
-
+#Each referee can officiate at most 1 match per day.
 @constraint(H21a, atMostOneMatchPerDay[r=1:R,d=1:D],
     sum(x[r,m]*match_time[m,d] for m=1:M) <= 1)
 
-
-    @constraint(H21a, refAvailability[r=1:R,m=1:M,d=1:D],
-        x[r,m]*match_time[m,d]  <= 1-ref_not_available[r,d])
+#Referees cannot officiate the days that they are not availalbe
+@constraint(H21a, refAvailability[r=1:R,m=1:M,d=1:D],
+    x[r,m]*match_time[m,d]  <= 1-ref_not_available[r,d])
 
 solve(H21a)
 
