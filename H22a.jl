7b2966744dbb24f920ba4fd2cdd787ac432f17e2
Nikolaos Bakoulas <46421526+NickBk92@users.noreply.github.com>
2019-01-25 04:31:48 +0100

    Update H22a.jl

--- H22a.jl
+++ H22a.jl
@@ -34,15 +34,13 @@ H22a = Model(solver=GurobiSolver())
 @variable(H22a, x[r=1:R,m=1:M], Bin)
 
 @objective(H22a, Max, sum(x[r,m]*ref_pair[r,rr]*x[rr,m] for r=1:R,m=1:M,rr=r:R))
-
+#Exactly two referees per match
 @constraint(H22a,twoRefPerMatch[m=1:M],
     sum(x[r,m] for r=1:R) == 2)
-
+#Each referee can officiate at most 1 match per day.
 @constraint(H22a, atMostOneMatchPerDay[r=1:R,d=1:D],
     sum(x[r,m]*match_time[m,d] for m=1:M) <= 1)
-
-
-
+#Referees cannot officiate the days that they are not availalbe
 @constraint(H22a, refAvailability[r=1:R,m=1:M,d=1:D],
     x[r,m]*match_time[m,d]  <= 1-ref_not_available[r,d])
 
@@ -51,4 +49,3 @@ solve(H22a)
 println(getobjectivevalue(H22a))
 
 target_pairs = getobjectivevalue(H22a)
-println(sum(getvalue(x[r,m])*referfee_arena_distances[r,a]*match_areana[m,a] for r=1:R,m=1:M,a=1:A))
