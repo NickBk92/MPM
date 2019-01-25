09d5ced170553d8da6213cff65ff1d04318c2955
Nikolaos Bakoulas <46421526+NickBk92@users.noreply.github.com>
2019-01-25 04:31:43 +0100

    Update H21b.jl

--- H21b.jl
+++ H21b.jl
@@ -12,8 +12,6 @@ using Printf
 include("H21a.jl")
 
 
-println(target_distance)
-
 #MODEL
 
 H21b = Model(solver=GurobiSolver())
@@ -21,21 +19,20 @@ H21b = Model(solver=GurobiSolver())
 @variable(H21b, x[r=1:R,m=1:M], Bin)
 
 @objective(H21b, Max, sum(x[r,m]*ref_pair[r,rr]*x[rr,m] for r=1:R,m=1:M,rr=r:R))
-
+#Exactly two referees per match
 @constraint(H21b,twoRefPerMatch[m=1:M],
     sum(x[r,m] for r=1:R) == 2)
-
+#Each referee can officiate at most 1 match per day.
 @constraint(H21b, atMostOneMatchPerDay[r=1:R,d=1:D],
     sum(x[r,m]*match_time[m,d] for m=1:M) <= 1)
-
-@constraint(H21b,dinstance_const
-    ,sum(x[r,m]*referfee_arena_distances[r,a]*match_areana[m,a] for r=1:R,m=1:M,a=1:A) <= target_distance)
+#Referees cannot officiate the days that they are not availalbe
 
 @constraint(H21b, refAvailability[r=1:R,m=1:M,d=1:D],
     x[r,m]*match_time[m,d]  <= 1-ref_not_available[r,d])
+#Find optimal pairs without worsen the distance.
+@constraint(H21b,dinstance_const,
+    sum(x[r,m]*referfee_arena_distances[r,a]*match_areana[m,a] for r=1:R,m=1:M,a=1:A) <= target_distance)
 
 solve(H21b)
 
 println(getobjectivevalue(H21b))
-
-println(sum(getvalue(x[r,m])*referfee_arena_distances[r,a]*match_areana[m,a] for r=1:R,m=1:M,a=1:A))
